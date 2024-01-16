a50ad88a611c1ae356be43896b5d4d50211f0552
laraturrion <laturrions@gmail.com>
2024-01-16 12:27:37 +0100

    usefetch

--- frontend/src/components/Catalogo/Catalogo.jsx
+++ frontend/src/components/Catalogo/Catalogo.jsx
@@ -1,7 +1,15 @@
-
+import { useFetch } from "../../useFetch";
 
 export const Catalogo = () => {
+  const {data} =useFetch("http://localhost:8080/api/hotels")
   return (
-    <div>Catalogo</div>
-  )
-}
+    <div className="Catalogo">
+      <h1>Fetch like a Pro</h1>
+      <ul>
+        {data?.map((hotel) => (
+          <li key={hotel.id}>{hotel.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
