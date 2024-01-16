6e7ec9aba6bef7f540b4091b866858dd760f8f30
laraturrion <laturrions@gmail.com>
2024-01-16 11:09:40 +0100

    header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import './header.css';
 
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="fondoHeader">
+    <header>
+      <div>
+        <p className="trooking">Trooking</p>
+      </div>
+      <div>
+        <button className="misReservas"> Mis Reservas </button>
+      </div>
+    </header>
+    </div>
+  );
+};
