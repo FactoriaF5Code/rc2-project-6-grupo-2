531fa32b4eaca713c07fe7d4622203199b7c1152
laraturrion <laturrions@gmail.com>
2024-01-17 15:12:05 +0100

    css mis reservas

--- frontend/src/components/MisReservas/MisReservas.jsx
+++ frontend/src/components/MisReservas/MisReservas.jsx
@@ -1,15 +1,23 @@
 import { useReservation } from "./ReservationContext";
+import { Header } from "../Header/Header";
+import Footer from "../Footer/Footer";
+import './MisReservas.css'
+
 
 function ConfirmacionReversa() {
-    const { reservationInfo } = useReservation();
+  const { reservationInfo } = useReservation();
   return (
-    <div>
-      {reservationInfo && (
-        <div className="reservation-message">
-          <p>{`¡Enhorabuena! Has reservado el hotel "${reservationInfo.hotelName}" por ${reservationInfo.price} €`}</p>
-        </div>
-      )}
-    </div>
+    <>
+      <Header />
+      <main className="contenedormisReservas">
+        {reservationInfo && (
+          <div className="reservation-message">
+            <p>{`¡Enhorabuena! Has reservado el hotel "${reservationInfo.hotelName}" por ${reservationInfo.price} €`}</p>
+          </div>
+        )}
+      </main>
+      <Footer />
+    </>
   );
 }
 
